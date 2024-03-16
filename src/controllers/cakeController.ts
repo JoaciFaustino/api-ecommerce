7978501170d76d1455f5f526eec65fef427958de
Joaci Faustino <joacifaustino@gmail.com>
2024-03-16 13:36:21 -0300

    [fix]: ajusted the message sent to the user when the attempt to search by ID in the database fails

--- src/controllers/cakeController.ts
+++ src/controllers/cakeController.ts
@@ -33,7 +33,7 @@ export class CakeController {
     const cake: CakeResponseDB = await cakeService
       .findById(id)
       .catch((error: any) => {
-        throw new ApiError("Failed to delete the cake", 400);
+        throw new ApiError("Failed to find the cake", 400);
       });
 
     res
