be63b261e49411a4d9f87c0e43c83f8f1dafaee0
Joaci Faustino <joacifaustino@gmail.com>
2025-03-06 15:29:39 -0300

    [fix]: fixed the sort by option "latest" in get all cakes route

--- src/@types/SortByCake.ts
+++ src/@types/SortByCake.ts
@@ -9,7 +9,7 @@ export type TypeKeysSortByCakes = (typeof SORT_BY_CAKES_OPTIONS)[number];
 
 export type typeSortByCakesObj =
   | { boughts: "descending" | "ascending"; _id: 1 }
-  | { created_at: "descending" | "ascending"; _id: 1 }
+  | { createdAt: "descending" | "ascending"; _id: 1 }
   | { totalPricing: "descending" | "ascending"; _id: 1 };
 
 export type typeSortByCakesOptions = {
@@ -18,7 +18,7 @@ export type typeSortByCakesOptions = {
 
 export const SORT_BY_CAKES_OBJS: typeSortByCakesOptions = {
   popularity: { boughts: "descending", _id: 1 },
-  latest: { created_at: "descending", _id: 1 },
+  latest: { createdAt: "descending", _id: 1 },
   price_high_to_low: { totalPricing: "descending", _id: 1 },
   price_low_to_high: { totalPricing: "ascending", _id: 1 }
 };
